8f1725471811a98f80312666983b6d93641c54e9
Clacson <cookiellacson@gmail.com>
2016-11-08 21:15:49 +0000

    ids matched to local variables

--- app/src/main/java/com/example/user/wordcounter/WordCounter.java
+++ app/src/main/java/com/example/user/wordcounter/WordCounter.java
@@ -3,6 +3,9 @@ package com.example.user.wordcounter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by user on 08/11/2016.
@@ -10,12 +13,21 @@ import android.util.Log;
 
 public class WordCounter extends AppCompatActivity{
 
+    EditText mQuestionEditText;
+    TextView mWordCountText;
+    Button mCoolButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         Log.d("WordCounter", "onCreate called and it wants its app back");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mQuestionEditText = (EditText) findViewById(R.id.sentence_text);
+        mWordCountText = (TextView) findViewById(R.id.word_count);
+        mCoolButton = (Button) findViewById(R.id.its_cool_button);
     }
 
 
