a589a84b88b4e5f0db4aa630bbff7524894fd4a5
Clacson <cookiellacson@gmail.com>
2016-11-08 16:39:44 +0000

    started countable interface and counter class

---
+++ app/src/main/java/com/example/user/wordcounter/Counter.java
@@ -0,0 +1,28 @@
+package com.example.user.wordcounter;
+
+import java.util.ArrayList;
+
+import Countable;
+
+/**
+ * Created by user on 08/11/2016.
+ */
+
+public class Counter extends Countable {
+
+    protected ArrayList<String> mWords;
+
+    public Counter(){
+        mWords = new ArrayList<String>();
+        getWords();
+    }
+
+    private void getWords(){
+
+    }
+
+    public int getWordCount(){
+
+    }
+
+}
