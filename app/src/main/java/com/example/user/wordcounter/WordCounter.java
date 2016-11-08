6a45e52aab9d878412b912c581e8528fd00d72ed
Clacson <cookiellacson@gmail.com>
2016-11-08 20:44:05 +0000

    onCreate completed, app started

--- app/src/main/java/com/example/user/wordcounter/WordCounter.java
+++ app/src/main/java/com/example/user/wordcounter/WordCounter.java
@@ -2,6 +2,7 @@ package com.example.user.wordcounter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
  * Created by user on 08/11/2016.
@@ -12,7 +13,7 @@ public class WordCounter extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
-        log.d("WordCounter", "onCreate called and it wants its app back")
+        Log.d("WordCounter", "onCreate called and it wants its app back");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
